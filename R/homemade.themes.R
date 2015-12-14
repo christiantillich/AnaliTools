47e43e9e8a9e81d53c7aaa001acf320158ff1eac
christian.tillich <christian.tillich@avant.com>
2015-12-14 14:27:53 -0600

    Explicit Name Space
    changing homemade.themes to have explicit namespace references because
    I'm getting pissed and need this working now.

--- R/homemade.themes.R
+++ R/homemade.themes.R
@@ -8,13 +8,13 @@ library(ggthemes)
 #' @return ggplot theme object, based off ggthemes::theme_gdocs()
 #' @export
 theme_BA <- function(){
-  x <- theme_gdocs()
+  x <- ggthemes::theme_gdocs()
   x$plot.title$size = 25
   x$plot.title$hjust = 0.5
   x$plot.title$vjust = 4
   x$axis.text.x$angle = 45
   x$axis.title.y$angle = 90
-  x$plot.margin = unit(c(0.1,0.1,0.1,0.1), 'npc')
+  x$plot.margin = grid::unit(c(0.1,0.1,0.1,0.1), 'npc')
   x$axis.title.x$vjust = -1
   x$axis.title.y$vjust = 2.5
 
