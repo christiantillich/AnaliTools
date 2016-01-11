3425bec99778d6661419e2dc290438fb6fbb04f4
christian.tillich <christian.tillich@avant.com>
2016-01-11 13:04:36 -0600

    Better documentation
    Document all the methods

--- R/FigR.R
+++ R/FigR.R
@@ -10,6 +10,7 @@
 #' printed out by FigR will have names 'fig001...', 'fig002...', and 'fig003...'.
 #' Defaults to 1
 #' @param type - file type to save as, as text. Defaults to .png.
+#' @export
 FigR <- function(write.path = getwd(), index = 1, type='.png'){
 
   #The object.
@@ -24,11 +25,24 @@ FigR <- function(write.path = getwd(), index = 1, type='.png'){
   return(me)
 }
 
-#Method to add image
+
+######## add.image method.
+
+#' add.image
+#' @description - Adds a plot object to the FigR container.
+#' @param self - An object of class FigR
+#' @param image - The ggplot object to add.
+#' @param name - The name to give the plot object
+#' @return Returns null
+#' @export
 add.image <- function(self, image, name) UseMethod('add.image',self)
+
+#' @export
 add.image.default <- function(self, image, name){
   stop('Argument 1 must be of class FigR')
 }
+
+#' @export
 add.image.FigR <- function(self, image, name){
 
   #Error if image is not a ggplot object.
@@ -42,21 +56,47 @@ add.image.FigR <- function(self, image, name){
   return(self)
 }
 
-#Method to remove image
+
+
+######## delete.image method.
+
+
+#' delete.image
+#' @description - Removes a plot object to the FigR container.
+#' @param self - An object of class FigR
+#' @param name.or.id - Either the name of the object, or its id.
+#' @return Returns null
+#' @export
 delete.image <- function(self, name.or.id) UseMethod('delete.image', self)
+
+#' @export
 delete.image.default <- function(self, name.or.id){
   stop('Argument 1 must be of class FigR')
 }
+
+#' @export
 delete.image.FigR <- function(self, name.or.id){
   self$images[name.or.id] <- NULL
   return(self)
 }
 
-#Method to print all images to write.path
+######## write.all method.
+
+
+#' write.all
+#' @description - Takes the FigR object and writes out all elements in
+#' $images to the directory specified by $write.path.
+#' @param self - An object of class FigR
+#' @return Returns null
+#' @export
 write.all <- function(self) UseMethod('write.all',self)
+
+#' @export
 write.all.default <- function(self){
   stop('Argument 1 must be of class FigR')
 }
+
+#' @export
 write.all.FigR <- function(self){
   if(length(self$images) == 0){stop('No plots to write. Please add some.')}
 
