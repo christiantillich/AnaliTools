281903ae184e1e4cbec01a97aaf281b5a442d33e
christian.tillich <christian.tillich@avant.com>
2016-01-12 17:54:40 -0600

    More Theme changes
    I have no clue what's going on

--- R/homemade.themes.R
+++ R/homemade.themes.R
@@ -10,20 +10,29 @@ library(ggthemes)
 theme_BA <- function(){
   x <- ggthemes::theme_gdocs()
   
-#   x$plot.title$vjust = 0
-  x$axis.text.x$angle = 45
-  x$axis.title.y$angle = 90
+  #Controls for the full plot margins
   x$plot.margin = grid::unit(c(0.1,0.1,0.1,0.1), 'npc')
   
+  #Controls for the main title text. 
   x$plot.title$size = 25
   x$plot.title$hjust = 0.35
   x$plot.title$margin = grid::unit(c(0,1,1,1), 'npc')
   
-  x$axis.title.x$vjust = -1
-  x$axis.title.y$vjust = 100
+  #Controls titles for the x and y axis. 
+  x$axis.title.y$angle = 90
   x$axis.title.y$hjust = 0.5
-  x$axis.title.x$margin = grid::unit(c(1,1,1,1), 'npc')
+  x$axis.title.y$vjust = 100
   x$axis.title.y$margin = grid::unit(c(0,1.5,0,0), 'npc')
+  
+  x$axis.title.x$vjust = -1
+  x$axis.title.x$margin = grid::unit(c(1,1,1,1), 'npc')
+  
+  #Controls for the axis tick labels. 
+  x$axis.text.x$vjust = 0.5
+  x$axis.text.x$angle = 45
+  
+  #Controls for the legend size. 
+  x$legend.key.size = grid::unit(0.8, 'lines')
 
   return(x)
 }
